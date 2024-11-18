7f470c82422089d00bef813d43ac623ac6d34856
elusivesmw <78516603+elusivesmw@users.noreply.github.com>
2024-11-17 20:37:02 -0800

    day 1 part 2

--- day1/main.go
+++ day1/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"os"
-	"strconv"
 	"strings"
 )
 
@@ -26,28 +25,72 @@ func main() {
 }
 
 func parseLine(line string) int {
-	var first rune
-	var last rune
-	for i, fr := range line {
-		li := len(line) - 1 - i
-		lr := rune(line[li])
+	var first int
+	var last int
+	first, last = getNums(line)
 
-		if first == 0 && isNum(fr) {
-			first = fr
-		}
-		if last == 0 && isNum(lr) {
-			last = lr
-		}
-	}
-	str := fmt.Sprintf("%s%s", string(first), string(last))
-
-	num, err := strconv.Atoi(str)
-	if err != nil {
-		return 0
-	}
-	return num
+	return first*10 + last
 }
 
 func isNum(r rune) bool {
 	return (r >= '0' && r <= '9')
+}
+
+var nums = [20]string{
+	"0",
+	"1",
+	"2",
+	"3",
+	"4",
+	"5",
+	"6",
+	"7",
+	"8",
+	"9",
+	"zero",
+	"one",
+	"two",
+	"three",
+	"four",
+	"five",
+	"six",
+	"seven",
+	"eight",
+	"nine",
+}
+
+type foundNum struct {
+	index int
+	value int
+	text  string
+}
+
+func newFoundNum() foundNum {
+	f := foundNum{index: -1, value: 0}
+	return f
+}
+
+func getNums(line string) (int, int) {
+	first := newFoundNum()
+	last := newFoundNum()
+
+	for i, num := range nums {
+		fi := strings.Index(line, num)
+		li := strings.LastIndex(line, num)
+
+		if fi != -1 && (fi < first.index || first.index == -1) {
+			first.index = fi
+			first.value = i % 10
+			//first.text = num
+			//fmt.Println("first", first)
+		}
+		if li != -1 && li > last.index {
+			last.index = li
+			last.value = i % 10
+			//last.text = num
+			//fmt.Println("last", last)
+		}
+	}
+
+	return first.value, last.value
 }
