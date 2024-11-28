5c091332fe2cb6af0e2897520ef032d86ab58600
elusivesmw <78516603+elusivesmw@users.noreply.github.com>
2024-11-28 11:58:34 -0800

    day 3

--- day3/main.go
+++ day3/main.go
@@ -4,36 +4,43 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
 )
 
 func main() {
-	data, err := os.ReadFile("sample.txt")
+	data, err := os.ReadFile("input.txt")
 	if err != nil {
 		panic(err)
 	}
-	lexer := newLexer(string(data))
-	fmt.Printf("lexer: %v\n", lexer)
+	input := string(data)
+	fmt.Printf("input:\n%s\n", input)
 
 	// get all tokens
-	tokens := tokenize(&lexer)
+	tokens := tokenize(&input)
 	total := 0
 
+	// print all tokens
 	//fmt.Printf("tokens: %v\n", tokens)
+	for _, token := range *tokens {
+		printToken(&token)
+	}
+	fmt.Println()
 
 	// add em up
 	for _, token := range *tokens {
 		printToken(&token)
-		if token.tokenType == Symbol {
-			continue
+		num := validNum(tokens, &token)
+		if num > 0 {
+			fmt.Printf("**%v is valid**\n\n", &token)
 		}
-
+		total += num
 		//line := token.pos / LineLen
 		//fmt.Printf("%v = line %d\n", token, line)
 	}
 	fmt.Printf("total: %d\n", total)
 }
 
-const LineLen = 10 // just hardcode it
+const LineLen = 140 // just hardcode it
 func validNum(tokens *map[int]token, token *token) int {
 	if hasAdjacentSymbol(tokens, token) {
 		n, err := strconv.Atoi(token.chars)
@@ -45,54 +52,70 @@ func validNum(tokens *map[int]token, token *token) int {
 	return 0
 }
 
-func hasAdjacentSymbol(tokens *map[int]token, star *token) bool {
+func isSymbolToken(token *token) bool {
+	return token.tokenType == Symbol
+}
+
+func validate(tokens *map[int]token, surrounding int) bool {
+	value, ok := (*tokens)[surrounding]
+	if !ok {
+		fmt.Printf("\t\tno token at %d\n", surrounding)
+		return false
+	}
+
+	if !isSymbolToken(&value) {
+		fmt.Printf("\t\ttoken is not symbol: %v\n", value.tokenType)
+		return false
+	}
+
+	return true
+}
+
+func hasAdjacentSymbol(tokens *map[int]token, center *token) bool {
 
 	//fmt.Printf("all tokens: %v\n", tokens)
 
-	for i, t := range *tokens {
-		fmt.Printf("at index %d, t = %v\n", i, t)
-		// dimiss other numbers
-		if t.tokenType == Number {
+	//fmt.Printf("at index %d, center = %v\n", center.pos, string(center.chars))
+	// only look at numbers
+	if center.tokenType != Number {
+		fmt.Println("\tcenter not a number")
+		return false
+	}
 
-			fmt.Println("  number")
+	// get row above
+	for i := center.pos - LineLen - 1; i < center.pos-LineLen+len(center.chars)+1; i++ {
+		fmt.Printf("\ti (surrounding based) = %d vs center = %d\n", i, center.pos)
+		if !validate(tokens, i) {
 			continue
 		}
+		fmt.Printf("\t\tmatch at i = %d\n", i)
+		return true
+	}
 
-		// get row above
-		for j := star.pos - LineLen - 1; j < star.pos-LineLen+star.len; j++ {
-			fmt.Printf("  j (star based) = %d vs i = %d\n", j, i)
-			if j == i {
-				fmt.Println("    match")
-				//if value, ok := tokens[i]
+	// get left of
+	lo := center.pos - 1
+	fmt.Printf("\tlo (surrounding based) = %d vs center = %d\n", lo, center.pos)
+	if validate(tokens, lo) {
+		fmt.Printf("\t\tmatch at lo = %d\n", lo)
+		return true
+	}
 
-				return true
-			}
+	// get right of
+	ro := center.pos + len(center.chars)
+	fmt.Printf("\tro (surrounding based) = %d vs i = %d\n", ro, center.pos)
+	if validate(tokens, ro) {
+		fmt.Printf("\t\tmatch at ro = %d\n", ro)
+		return true
+	}
+
+	// get row below
+	for i := center.pos + LineLen - 1; i < center.pos+LineLen+len(center.chars)+1; i++ {
+		fmt.Printf("\ti (surrounding based) = %d vs i = %d\n", i, center.pos)
+		if !validate(tokens, i) {
+			continue
 		}
-
-		// get left of
-		ab := star.pos - 1
-		fmt.Printf("  ab (star based) = %d vs i = %d\n", ab, i)
-		if ab == i {
-			fmt.Println("    match")
-			return true
-		}
-
-		// get right of
-		aa := star.pos + star.len
-		if aa == i {
-			fmt.Printf("  aa (star based) = %d vs i = %d\n", aa, i)
-			fmt.Println("    match")
-			return true
-		}
-
-		// get row below
-		for j := star.pos + LineLen + 1; j < star.pos+LineLen+star.len; j++ {
-			fmt.Printf("  j (star based) = %d vs i = %d\n", j, i)
-			if j == i {
-				fmt.Println("    match")
-				return true
-			}
-		}
+		fmt.Printf("\t\tmatch at i = %d\n", i)
+		return true
 	}
 	return false
 }
@@ -101,89 +124,76 @@ func validIndex(i int, length int) bool {
 	return i >= 0 && i < length
 }
 
-type lexer struct {
-	runes []rune
-	pos   int
+func peek(line string, pos int) rune {
+	return peekAt(line, pos, 0)
 }
 
-func newLexer(input string) lexer {
-	return lexer{
-		runes: []rune(input),
-		pos:   0,
-	}
-}
-
-func peek(lexer *lexer) rune {
-	return peekAt(lexer, 0)
-}
-
-func peekAt(lexer *lexer, offset int) rune {
-	pos := lexer.pos + offset
-	if pos >= len(lexer.runes) {
+func peekAt(line string, pos int, offset int) rune {
+	next := pos + offset
+	if next >= len(line) {
 		return 0
 	}
-	return lexer.runes[pos]
+	return []rune(line)[next]
 }
 
-func skipSpace(lexer *lexer) {
-	p := peek(lexer)
-	for p == '.' || p == '\n' {
-		lexer.pos++
-		p = peek(lexer)
-	}
+func printCurrentRune(r rune) {
+	fmt.Printf("r: %s\n", string(r))
 }
 
-func printCurrentRune(lexer *lexer) {
-	fmt.Printf("lexer.runes[%d] = %d = %s\n", lexer.pos, lexer.runes[lexer.pos], string(lexer.runes[lexer.pos]))
-}
-
-func tokenize(lexer *lexer) *map[int]token {
+func tokenize(input *string) *map[int]token {
 	tokens := make(map[int]token)
 
-	for lexer.pos < len(lexer.runes) {
-		skipSpace(lexer)
-		r := peek(lexer)
-		if r == 0 {
-			break
-		}
+	lines := strings.Split(*input, "\n")
+	for i, line := range lines {
+		current := token{}
 
-		key := lexer.pos
-		current := token{
-			pos: lexer.pos,
-		}
+		j := 0
+		for j < len(line) {
+			r := peek(line, j)
+			absPos := i*LineLen + j
+			//fmt.Printf("pos: %d\n", absPos)
 
-		offset := 0
-		if isNum(r) {
-			current.tokenType = Number
+			offset := 0
+			if isNum(r) {
+				current.tokenType = Number
+				current.pos = absPos
 
-			var num []rune
-			next := peek(lexer)
-			for isNum(next) {
-				//printCurrentRune(lexer)
-				num = append(num, next)
+				var num []rune
+				next := peek(line, j)
+				for isNum(next) {
+					//printCurrentRune(next)
+					num = append(num, next)
+					//fmt.Printf("num in progress: %s\n", string(num))
 
-				lexer.pos++
-				next = peekAt(lexer, offset)
+					offset++
+					next = peekAt(line, j, offset)
+				}
+				current.chars = string(num)
+
+				tokens[absPos] = current
+				current = token{}
+			} else if isSymbol(r) {
+				//printCurrentRune(r)
+				current.tokenType = Symbol
+				current.pos = absPos
+
+				offset++
+				sym := []rune(string(r))
+				current.chars = string(sym)
+
+				tokens[absPos] = current
+				current = token{}
+			} else {
+				// '.'
+				offset++
 			}
-			current.len = len(num)
-			current.chars = string(num)
-
-			tokens[key] = current
-
-		} else if isSymbol(r) {
-			//printCurrentRune(lexer)
-			current.tokenType = Symbol
-
-			offset++
-			sym := []rune(string(r))
-			current.len = len(sym)
-			current.chars = string(sym)
-
-			tokens[key] = current
-
+			//fmt.Printf("offset here is %d\n", offset)
+			j += offset
 		}
-		lexer.pos += offset
+		i++
+
 	}
+
 	return &tokens
 }
 
@@ -198,17 +208,18 @@ type token struct {
 	tokenType TokenType
 	chars     string
 	pos       int
-	len       int
 }
 
 func isNum(r rune) bool {
+	//fmt.Printf("r = %s is num \n", string(r))
 	return r >= '0' && r <= '9'
 }
 
 func isSymbol(r rune) bool {
+	//fmt.Printf("r = %s is sym \n", string(r))
 	return r != '.' && !isNum(r)
 }
 
 func printToken(token *token) {
-	fmt.Printf("%s\tis a %s at %d (len of %d)\n", token.chars, token.tokenType, token.pos, token.len)
+	fmt.Printf("%s\tis a %s at %d (len of %d)\n", token.chars, token.tokenType, token.pos, len(token.chars))
 }
